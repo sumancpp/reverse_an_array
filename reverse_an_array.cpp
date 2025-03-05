15ff61b26c388f3856d8dd68140705f2c9497797
Suman Maity <someone.cpp@proton.me>
2025-03-05 21:05:01 +0530

    Update reverse_an_array.cpp

--- reverse_an_array.cpp
+++ reverse_an_array.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
-void reverse(int arr[],int size){
-    int start=0,end=size-1;
-    while(start<end){
-        swap(arr[start],arr[end]);
+void reverse(int arr[], int size){
+    int start = 0, end = size - 1;
+    while(start < end) {
+        swap(arr[start], arr[end]);
         start++;
         end--;
     }
@@ -11,8 +11,8 @@ void reverse(int arr[],int size){
 int main(){
     int arr[]={1,2,3,4,5};
     int size=5;
-    reverse(arr,size);
-    for(int i=0;i<size;i++){
+    reverse(arr, size);
+    for(int i=0; i<size; i++){
       cout<<arr[i]<<endl;
     }
     return 0;
